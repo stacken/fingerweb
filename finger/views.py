63cca892027d527b0494bac98bf081bf6631c355
Rasmus Kaj <kaj@kth.se>
2018-03-10 23:12:10 +0100

    Add service descriptions.
    Add a long and a short description for each service.  The short one is
    used in the service list (and show in the admin overview).  The long
    one is showed on the service page and contains markdown (so it can
    e.g. link to the actual service).
    
    Also add number of created accounts to admin overview.

--- finger/views.py
+++ finger/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from services.models import Service
 
 @login_required
 def index(request):
     return render(request, 'home.html', {
+        'services': Service.objects.all()
     })
--- services/admin.py
+++ services/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Service
 
-admin.site.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'shortdesc', 'nr_users')
+
+
+admin.site.register(Service, ServiceAdmin)
