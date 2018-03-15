3d44f38364d5bd63742cadd7f369b24d1ae0ac75
Rasmus Kaj <kaj@kth.se>
2018-03-15 20:28:15 +0100

    Require a POST to show password.

--- services/views.py
+++ services/views.py
@@ -5,14 +5,14 @@ from .models import Service
 # Create your views here.
 def service(request, name):
     service = get_object_or_404(Service, name=name)
-    if request.POST.get('action') == 'generate':
+    action = request.POST.get('action')
+    show_secret = action in ('generate', 'show')
+    if action == 'generate':
         service.generate_password(request.user)
-        return redirect(request.path + "?show-secret=t")
-    else:
-        account = service.account_for(request.user)
-        show_secret = request.GET.get('show-secret')
-        return render(request, 'services/service.html', {
-            'service': service,
-            'has_account': account and True,
-            'secret': show_secret and account and account.secret,
-        })
+
+    account = service.account_for(request.user)
+    return render(request, 'services/service.html', {
+        'service': service,
+        'has_account': account and True,
+        'secret': show_secret and account and account.secret,
+    })
