d0bca918ecc99cd4d9d3f97863ba48627adf6fa3
Rasmus Kaj <kaj@kth.se>
2018-04-05 20:46:18 +0200

    Create admin account.
    Allways create / reset admin account on app startup if a password
    is given in environment.

---
+++ finger/management/commands/stackenadmin.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from optparse import make_option
+from finger.models import User
+from os import environ
+
+class Command(BaseCommand):
+    help = 'Find and read user info from finger.json'
+
+    def handle(self, **options):
+        password = environ.get('DJANGOADMIN_PASSWORD')
+        if not password:
+            print("No password given.  No admin created")
+            return
+        User.objects.filter(username='admin').delete()
+        User.objects.create_superuser('admin', 'staff@stacken.kth.se', password)
+        print("Created/updated admin accout")
