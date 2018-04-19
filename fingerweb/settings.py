499f9a480cad8a59fe262256f79c33799b1c8cc1
Rasmus Kaj <kaj@kth.se>
2018-04-19 20:57:25 +0200

    Set DEFAULT_FROM_EMAIL

--- fingerweb/settings.py
+++ fingerweb/settings.py
@@ -16,6 +16,7 @@ ADMINS = (
     ('Stacken', 'staff@stacken.kth.se'),
 )
 MANAGERS = ADMINS
+DEFAULT_FROM_EMAIL = 'staff@stacken.kth.se'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
