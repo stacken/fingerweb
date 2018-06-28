6040943bc2869cbfcd2a433f79935a2226689916
Stefan Berggren <nsg@nsg.cc>
2018-06-28 22:14:05 +0200

    Use our own ModelAdmin for Users in admin.

--- finger/admin.py
+++ finger/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class StackenUserAdmin(admin.ModelAdmin):
+    list_display = ('username',
+                    'first_name',
+                    'last_name',
+                    'payed_year')
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'first_name',
+                'last_name',
+                'email'
+                )
+        }),
+        ('Medlemsstatus', {
+            'fields': (
+                'payed_year',
+                ('ths_claimed_vt', 'ths_claimed_ht'),
+                ('ths_verified_vt', 'ths_verified_ht'),
+                'date_joined'
+                )
+        }),
+        ('Admin', {
+            'fields': (
+                'is_superuser',
+                'is_staff',
+                'is_active'
+            )
+        }),
+    )
+
+
+admin.site.register(User, StackenUserAdmin)
