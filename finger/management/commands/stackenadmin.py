905067496bde7d1da631cf525088a4ee51566a7b
Stefan Berggren <nsg@nsg.cc>
2020-11-08 13:58:51 +0100

    Try to fix the state left in the DB before PR #28

--- finger/management/commands/stackenadmin.py
+++ finger/management/commands/stackenadmin.py
@@ -15,6 +15,14 @@ class Command(BaseCommand):
         if not password:
             print("No password given. No admin created/updated")
             return
+
+        # Fix for developers, PR #28 fixed an bug where is_active and
+        # data_joined was not set propperly. This code can be removed
+        # in the future.
+        if User.objects.filter(username="admin", is_active=None).exists():
+            print("Admin user needs to be recreated")
+            User.objects.filter(username="admin").delete()
+
         if User.objects.filter(username="admin").exists():
             print("Admin user already exists, update the existing user")
             User.objects.get(username="admin").set_password(password)
