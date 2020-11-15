d95500b2e1a2d5efd92fe4ce6c4dfa03c3d9693e
Stefan Berggren <nsg@nsg.cc>
2020-11-15 12:54:57 +0100

    Add ServiceUser to Django Admin

--- services/admin.py
+++ services/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Service
+from .models import ServiceUser
 
 
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ("name", "shortdesc", "nr_users")
 
 
+class ServiceUserAdmin(admin.ModelAdmin):
+    list_display = ("service", "user", "modified")
+
+
 admin.site.register(Service, ServiceAdmin)
+admin.site.register(ServiceUser, ServiceUserAdmin)
